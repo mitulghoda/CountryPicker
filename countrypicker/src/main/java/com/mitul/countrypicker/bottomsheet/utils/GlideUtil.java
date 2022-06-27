ed3a25e8345d94f85f30b69eb416fbd26c0d846d
mitul.devstree <mitul.devstree@gmail.com>
2022-06-27 17:31:47 +0530

    Open bottomsheet in fullscreen

--- countrypicker/src/main/java/com/mitul/countrypicker/bottomsheet/utils/GlideUtil.java
+++ countrypicker/src/main/java/com/mitul/countrypicker/bottomsheet/utils/GlideUtil.java
@@ -18,42 +18,14 @@ public class GlideUtil {
         }
         return defaultRequestOption;
     }
-
-    public static RequestOptions getDefaultRequestOption(int res_id) {
-        return BuildRequestOptionRounded(res_id);
-    }
-
     @BindingAdapter("loadImage")
     public static void loadImage(ImageView view, String imageUrl) {
         Glide.with(view.getContext()).load(imageUrl).apply(GlideUtil.getDefaultRequestOption()).into(view);
     }
-
-    public static RequestOptions BuildRequestOptionNormal(int image_holder_portrait) {
-        return new RequestOptions().placeholder(image_holder_portrait)
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).error(image_holder_portrait);
-    }
-
     public static RequestOptions BuildRequestOptionRounded(int image_holder_portrait) {
         return new RequestOptions().placeholder(image_holder_portrait)
                 .error(image_holder_portrait)
                 .fallback(image_holder_portrait)
                 .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);
-    }
-
-    public static RequestOptions BuildImageLoader(int image_holder_portrait) {
-        return new RequestOptions().placeholder(image_holder_portrait)
-                .error(image_holder_portrait)
-                .fallback(image_holder_portrait)
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);
-    }
-
-    public static RequestOptions BuildRequestOptionRounded() {
-        return new RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);
-    }
-
-
-    public static RequestOptions BuildRequestVideoCenterCrop(int image_holder_portrait) {
-        return new RequestOptions().placeholder(image_holder_portrait).centerCrop()
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).error(image_holder_portrait);
     }
 }
